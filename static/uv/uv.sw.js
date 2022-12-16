ccdf7828570934ff650a672acf9869fc338d5145
WmeLuna <46545277+WmeLuna@users.noreply.github.com>
2022-12-16 14:11:53 -0500

    Replace Font in error

--- static/uv/uv.sw.js
+++ static/uv/uv.sw.js
@@ -191,7 +191,7 @@ class UVServiceWorker extends EventEmitter {
             const head = new Headers({
               "Content-Type": "text/html; charset=utf-8"
             })
-            return new Response("<b>Unblocker is down</b></br>Service Worker error:</br>" + err.toString(), {
+            return new Response("<style>body{font-family:Helvetica,system-ui}</style><b>Unblocker is down</b></br>Service Worker error:</br>" + err.toString(), {
                 status: 500,
                 headers: head
             });
